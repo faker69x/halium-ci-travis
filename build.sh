7d3209eef6ca1aa076a7d6481ecd55a2b0d185c1
LNJ <git@lnj.li>
2018-01-16 12:49:46 +0100

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+# init the hailum/android repos
+repo init -u https://github.com/Halium/android.git -b halium-7.1 --depth=1
+
+# clone halium-devices
+mkdir halium
+cd halium
+git clone https://github.com/halium/halium-devices --branch=halium-7.1 --depth=1
+cd ..
+
+JOBS=15 ./halium/halium-devices/setup $HALIUM_DEVICE
+
+# setup for building
+source build/envsetup.sh
+breakfast $HALIUM_DEVICE
+
+# compile and build everything
+mka mkbootimg
+mka hybris-boot
+mka systemimage
+
